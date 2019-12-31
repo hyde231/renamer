2f71d65460b5b587a00e6352b7fe4b49ba6e394b
ChalupaBatman <chalupa@metadataapi.net>
2019-12-31 13:17:16 +1100

    re-write using commander.js

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
-import {Command, flags} from "@oclif/command";
 import axios from "axios";
-
+import {cli} from "cli-ux";
 import walk from "./walk";
 
+const program = require("commander");
 const builder = require("xmlbuilder");
 const util = require("util");
 const path = require("path");
 const inquirer = require("inquirer");
 const fs = require("fs");
 const mkdirp = require("mkdirp");
-
-type Background = {
-    small: string
-    medium: string
-    large: string
-}
+const stringFormat = require("string-template");
+require("./types");
 
 type Performer = {
     name: string
@@ -23,6 +19,7 @@ type Performer = {
 }
 type Site = {
     name: string
+    id: string
 }
 type Tag = {
     id: number
@@ -44,29 +41,31 @@ type Scene = {
     tags: Array<Tag>
 }
 
-class PornRenamer extends Command {
-    static description = "describe the command here";
+type Background = {
+    large: string
+    medium: string
+    small: string
+}
 
-    static flags = {
-        // add --version flag to show CLI version
-        version: flags.version({char: "v"}),
-        help: flags.help({char: "h"}),
-        // flag with a value (-n, --name=VALUE)
-        directory: flags.string({char: "d", description: "Directory to rename"}),
-        // flag with no value (-f, --force)
-        force: flags.boolean({char: "f"})
-    };
 
-    // static args = [{name: 'file'}];
+class PornRenamer {
+    private program: any;
+
+    constructor(program: any) {
+        this.program = program;
+    }
 
     async run() {
-        const {args, flags} = this.parse(PornRenamer);
-        const dir = flags.directory || process.cwd();
-
+        const dir = this.program.directory;
         const files: Array<string> = [];
         await walk(dir, [".mkv", "avi", "wmv", "mp4"], async (file: string) => {
             files.push(file);
         });
+
+        if (files.length === 0) {
+            cli.error("No files found, exiting");
+            cli.exit();
+        }
 
         for (const file of files) {
 
@@ -88,7 +87,7 @@ class PornRenamer extends Command {
                     searchTerm = match[1] + " " + match[3].split(".").join(" ") + " " + match[4].split(", ").join(" ");
                 }
             } else {
-                this.warn("Couldn't parse file: " + file);
+                cli.warn("Couldn't parse file: " + file);
                 continue;
             }
 
@@ -98,7 +97,7 @@ class PornRenamer extends Command {
 
             let rsp = await this.searchApi(searchTerm);
             if (!rsp.length) {
-                this.warn("No search results for " + file);
+                cli.warn("No search results for " + file);
                 continue;
             }
 
@@ -133,26 +132,32 @@ class PornRenamer extends Command {
                 uuid = rsp[0]["id"];
             }
 
-            let scene = await this.getScene(uuid);
+            const scene = await this.getScene(uuid);
 
-            const fileName = util.format(
-                "%s/%s - %s - %s [%s]",
-                scene.site.name,
-                scene.site.name,
-                scene.title,
-                scene.date,
-                scene.performers.map(i => i.name).join(", ")
-            );
+            // const fileName = util.format(
+            //     "%s/%s - %s - %s [%s]",
+            //     scene.site.name,
+            //     scene.site.name,
+            //     scene.title,
+            //     scene.date,
+            //     scene.performers.map(i => i.name).join(", ")
+            // );
 
-            console.log("rename " + file + " to " + path.join(dir, fileName, path.extname(file)));
+            const fileName = stringFormat(program.format, {
+                title: scene.title,
+                site: scene.site.name,
+                date: scene.date,
+                performers: scene.performers.map(i => i.name).join(', ')
+            });
 
-            console.log(path.join(dir, fileName, ".nfo"));
+            cli.log("rename to " + path.join(dir, fileName) + path.extname(file));
+
             const nfoFileName = path.join(dir, fileName) + ".nfo";
             const folder = path.dirname(nfoFileName);
 
             mkdirp(folder);
-            fs.writeFile(path.join(dir, fileName) + ".nfo", await this.generateNfo(scene), () => {
-                this.log("NFO Created");
+            fs.writeFile(path.join(dir, fileName) + ".nfo", await this.generateNfo(scene, program.kodi), () => {
+                cli.log("NFO Created");
             });
 
         }
@@ -162,45 +167,91 @@ class PornRenamer extends Command {
     /**
      * @param search
      */
-    async searchApi(search: string) {
+    async searchApi(search: string): Promise<Array<Scene>> {
         let rsp = await axios.get("https://metadataapi.net/api/scenes?q=" + search);
         return rsp.data.data;
     }
 
-    async getScene(uuid: string) {
+    async getScene(uuid: string): Promise<Scene> {
         let rsp = await axios.get("https://metadataapi.net/api/scenes/" + uuid);
 
         return rsp.data.data;
     }
 
-    async generateNfo(scene: Scene): Promise<string> {
-        const obj = {
-            scene: {
-                title: scene.title,
-                studio: scene.site.name,
-                description: scene.description,
-                released: scene.date,
-                thumb: scene.background.large,
-                poster: scene.poster,
-                performers: {
+    async generateNfo(scene: Scene, kodi: boolean = false): Promise<string> {
+        let obj = {};
+
+        if (kodi) {
+            obj = {
+                movie: {
+                    title: scene.title,
+                    studio: scene.site.name,
+                    plot: scene.description,
+                    premiered: scene.date,
+                    thumb: [
+                        {
+                            "#text": scene.background.large,
+                            "@aspect": "landscape"
+                        },
+                        {
+                            "#text": scene.poster,
+                            "@aspect": "poster"
+                        }
+                    ],
+
+                    poster: scene.poster,
                     performer: scene.performers.map(i => {
                         return {
                             name: i.name,
-                            image: i.image,
-                            id: i.id
+                            thumb: i.image,
+                            role: i.name
                         };
-                    })
-                },
-                tags: {
-                    tag: scene.tags.map(t => t.tag)
+                    }),
+                    genre: scene.tags.map(t => t.tag),
+                    uniquid: {
+                        "@type": "tpdb",
+                        "#text": scene.id
+                    }
                 }
-            }
-        };
+            };
+        } else {
+            obj = {
+                scene: {
+                    title: scene.title,
+                    studio: scene.site.name,
+                    description: scene.description,
+                    released: scene.date,
+                    thumb: scene.background.large,
+                    poster: scene.poster,
+                    performers: {
+                        performer: scene.performers.map(i => {
+                            return {
+                                name: i.name,
+                                image: i.image,
+                                id: i.id
+                            };
+                        })
+                    },
+                    tags: {
+                        tag: scene.tags.map(t => t.tag)
+                    }
+                }
+            };
+        }
 
         return builder.create(obj).end({pretty: true});
     }
-
-
 }
 
-export = PornRenamer
+let command = program
+    .version("0.0.1")
+    .requiredOption("-d, --directory <directory>", "Directory to scan for files", process.cwd())
+    .option("-k, --kodi", "Outputs the NFO using Kodi Movie standard", false)
+    .option("-f, --format <format>", "The format of the filenames", "{site} - {title} - {date} [{performers}]")
+    .parse(process.argv);
+
+//console.log(command);
+
+let pr = new PornRenamer(command);
+pr.run();
+
---
+++ src/types.ts
@@ -0,0 +1,34 @@
+type Performer = {
+    name: string
+    image: string
+    id: string
+}
+type Site = {
+    name: string
+    id: string
+}
+type Tag = {
+    id: number
+    tag: string
+}
+type Scene = {
+    id: string
+    title: string
+    description: string
+    site_id: number
+    date: string
+    url: string
+    poster: string
+    background: Background,
+    created: string
+    last_updated: string
+    performers: Array<Performer>
+    site: Site
+    tags: Array<Tag>
+}
+
+type Background = {
+    large: string
+    medium: string
+    small: string
+}
